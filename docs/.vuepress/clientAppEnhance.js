9fdced11d2476769b6160a91f738fd2304dd6b37
shengxinjing <316783812@qq.com>
2021-12-03 15:03:55 +0800

    change vite in vuepress@v2

--- docs/.vuepress/clientAppEnhance.js
+++ docs/.vuepress/clientAppEnhance.js
@@ -8,13 +8,17 @@ export default defineClientAppEnhance(({ app, router, siteData }) => {
   app.component('roadmap', RoadMap)
 
   // baidutongji
-  var _hmt = _hmt || [];
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ccf55dfd2764cf3ebf43d6b3c9da9b20";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
 
-  router.afterEach(function (to) {
-    _hmt.push(['_trackPageview', to.fullPath])
-  })
+  if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
+    var _hmt = _hmt || [];
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?ccf55dfd2764cf3ebf43d6b3c9da9b20";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  
+    router.afterEach(function (to) {
+      _hmt.push(['_trackPageview', to.fullPath])
+    })
+  }
+
 })
